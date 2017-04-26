39a648f1e45d98184caf3f14d4d661b905e59d09
Gonçalo Girão <gbg@MBP-Girao.local>
2017-04-26 12:16:05 +0100

    image xib changed to IF_news #2

--- Common/Controllers/CategoryViewController.m
+++ Common/Controllers/CategoryViewController.m
@@ -53,6 +53,32 @@ NewsItemTableViewCellDelegate, MenuItemTableViewCellDelegate>
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+//    UITableViewController *tableViewController = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
+//    tableViewController.tableView = self.contentTableView;
+//    
+//    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+//    [refreshControl addTarget:self action:@selector(handleRefresh:) forControlEvents:UIControlEventValueChanged];
+//    [self.contentTableView addSubview:refreshControl];
+//
+//    
+//    if([self.contentTableView respondsToSelector:@selector(setRefreshControl:)]) {
+//        [self.contentTableView setRefreshControl:refreshControl];
+//    }
+//    else {
+//        [self.contentTableView addSubview:refreshControl];
+//    }
+//
+//    //Setting attribute Title
+//    refreshControl.attributedTitle = [[NSAttributedString alloc]initWithString:@"Pull To Refresh"];
+//    //[self.contentTableView setContentOffset:CGPointMake(0, -refreshControl.frame.size.height) animated:YES];
+//    //[self.contentTableView setContentOffset:CGPointMake(0.0f, -0.0f)
+//    //                               animated:YES];
+//    
+//    [refreshControl beginRefreshing];
+//    [self performSelector:@selector(handleRefresh:) withObject:nil afterDelay:2];
+//    
+//    [self pullToRefresh];
+    
     self.allMenuItems = [MenuItem sortedMenuItems];
     self.newsItems = [NewsItem MR_findAll];
     
@@ -79,8 +105,30 @@ NewsItemTableViewCellDelegate, MenuItemTableViewCellDelegate>
     
     self.activeCategoryId = self.navigationId;
     
+    
     [self refreshCategory:[self.activeCategoryId intValue]];
+    
 }
+
+
+//- (void) pullToRefresh
+//{
+//    [self refreshCategory:[self.activeCategoryId intValue]];
+//    
+//    [self performSelector:@selector(handleRefresh:) withObject:nil afterDelay:2];
+//}
+//
+//- (void)handleRefresh:(UIRefreshControl *)refreshControl {
+//    
+//        NSLog(@"REFRESHING!");
+//    
+//        //[refreshControl beginRefreshing];
+//        //[refreshControl layoutIfNeeded];
+//        [self.contentTableView reloadData];
+//        [self.contentTableView layoutIfNeeded];
+//    
+//        [refreshControl endRefreshing];
+//}
 
 - (IBAction)openInfoReport:(id)sender {
     UIViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"infoReportViewController"];
