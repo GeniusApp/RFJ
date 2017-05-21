ee6c0da84cfb62f712ce76ffbcba01f6ab5cd4f4
Gonçalo Girão <gbg@MBP-Girao.local>
2017-05-21 21:38:27 +0100

    Menu indentation fixed #1_4

--- Common/Controllers/NewsGroupViewController.m
+++ Common/Controllers/NewsGroupViewController.m
@@ -219,7 +219,57 @@
                 
                 actualCell.delegate = self;
                 
-                [actualCell setName:item.name];
+                if ([item.name  isEqual: @"Région"]) {
+                    item.name = @"   Région";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Suisse"]) {
+                    item.name = @"   Suisse";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Monde"]) {
+                    item.name = @"   Monde";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Économie"]) {
+                    item.name = @"   Économie";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Culture"]) {
+                    item.name = @"   Culture";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Football"]) {
+                    item.name = @"   Football";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Hockey"]) {
+                    item.name = @"   Hockey";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Basketball"]) {
+                    item.name = @"   Basketball";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Volleyball"]) {
+                    item.name = @"   Volleyball";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Cyclisme"]) {
+                    item.name = @"   Cyclisme";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Ski"]) {
+                    item.name = @"   Ski";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Hippisme"]) {
+                    item.name = @"   Hippisme";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Tennis"]) {
+                    item.name = @"   Tennis";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Autres sports"]) {
+                    item.name = @"   Autres sports";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Sports motorisés"]) {
+                    item.name = @"   Sports motorisés";
+                    [actualCell setName:item.name];
+                } else if ([item.name  isEqual: @"Inline hockey"]) {
+                    item.name = @"   Inline hockey";
+                    [actualCell setName:item.name];
+                } else {
+                    [actualCell setName:item.name];
+                }
                 
                 BOOL shouldExpand = [self.allMenuItems indexOfObjectPassingTest:^BOOL(MenuItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
                     return obj.parentId == item.id;
